5f0eec658661ba271cbe1a82bdf4b921260c3b49
Rahul Behl <raulbehl@gmail.com>
2022-12-07 07:33:32 +0530

    Include d3-wave

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { WaveGraph } from 'd3-wave';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script type="text/javascript">
+        var svg = d3.select("#wave-graph");
+        var waveGraph = new WaveGraph(svg);
+        waveGraph.setSizes();
+        waveGraph.bindData(JSON.parse(contents));
+      </script>
     </div>
   );
 }
